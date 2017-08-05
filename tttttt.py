fb7605b23aff31df6f62b9ef245519509783ac0c
naive but not ignorant <335320567@qq.com>
2017-08-05 09:49:59 +0800

    Delete tttttt.py

--- tttttt.py
+++
@@ -1,7 +0,0 @@
-import os,sys
-xls_dir = os.path.split(os.path.realpath(sys.argv[0]))[0] + "\\logs\\longping\\"
-xlsfile = os.path.join(xls_dir,"longpingdata.xlsx")
-print(xlsfile)
-if os.path.exists(xlsfile):
-    print("11")
-    os.system(xlsfile)
